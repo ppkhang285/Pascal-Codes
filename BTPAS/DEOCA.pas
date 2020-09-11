215fc7762c11f225f643d2d9aa33c268223f5bc9
Phạm Khang <ppkhang285@gmail.com>
2020-09-11 16:41:22 +0700

    Up DEOCA.pas - AC

--- BTPAS/DEOCA.pas
+++ BTPAS/DEOCA.pas
@@ -1,33 +1,26 @@
 const fi ='DEOCA.INP';fo = 'DEOCA.OUT';
-var A,B,F:array[0..100005] of longint;
-    n,res1:longint;
+var A,B,ID,F:array[0..100005] of longint;
+    n,res1,res2:longint;
 procedure nhap;
- var i:longint;
+ var i,X:longint;
  begin
         assign(input,fi);reset(input);
         assign(output,fo);rewrite(output);
                 readln(n);
-                for i:=1 to n do read(A[i]);
-                readln;
-                for i:=1 to n do read(B[i]);
- end;
-function search(L,R,X:longint):longint;
- var  mid,res:longint;
- begin
-        res:=0;
-        while (L<=R) do
-         begin
-                mid:= (L+R) div 2;
-                if (F[mid] >= X) then
+                for i:=1 to n do
                  begin
-                        res:= mid;
-                        R:= mid-1;
-                 end
-                else L:=mid+1;
-         end;
-       if (res = 0) then exit(1) else
-                exit(res);
+                        read(x);
+                        A[i]:=i;
+                        ID[x]:=i;
+                 end;
+                readln;
+                for i:=1 to n do
+                 begin
+                        read(x);
+                        B[i]:= ID[x];
+                 end;
  end;
+
 procedure solve1;
  var  i:longint;
  begin
@@ -39,19 +32,44 @@ function max(x,y:longint):longint;
  begin
         if (x>y) then exit(x) else exit(y);
  end;
+function Get(i:longint):longint;
+ var tp:longint;
+ begin
+        tp:=0;
+        while (i>=1) do
+         begin
+              tp:= tp + F[i];
+              i:=i - ( i and (-i) );
+         end;
+        exit(tp);
+ end;
+procedure Update(i:longint);
+ begin
+        while (i<=n) do
+         begin
+                F[i]:= F[i] + 1;
+                i:= i + (i and (-i) );
+         end;
+ end;
 procedure solve2;
  var  m,i,k:longint;
  begin
-        m:=0;
-        for i:=1 to n do
+        res2:=0;
+        for i:=n downto 1 do
          begin
-                k:= search(1,m , A[i]) ;
-                m:= max(m , k);
-                F[k]:= A[i];
+                update(B[i]);
+                if (B[i] = 1 ) then continue;
+                if (Get(B[i]-1) >0 ) then res2:=res2 +1;
          end;
+ end;
+procedure xuat;
+ begin
+        writeln(res1);
+        writeln(res2);
  end;
 BEGIN
         nhap;
-        //solve1;
+        solve1;
         solve2;
+        xuat;
 END.
